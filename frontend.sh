dd9e3e283ee72be95b23bf607d74380abe55b3d7
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-10-14 07:20:20 +0530

    updating redirectors

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,16 @@
-yum install nginx -y
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e[32mInstalling Nginx Server\e[0m"
+yum install nginx -y >/tmp/roboshop.log
+
+echo -e "\e[32mRemoving Old Content\e[0m"
+rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
+
+echo -e "\e[32mDownloading FrontEnd Content\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+
+echo -e "\e[32mExtracting FrontEnd Content\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl restart nginx
+unzip /tmp/frontend.zip >/tmp/roboshop.log
+
+echo -e "\e[32mRestarting Nginx Service\e[0m"
+systemctl enable nginx >/tmp/roboshop.log
+systemctl restart nginx >/tmp/roboshop.log
