924aa3357e7d3c600b5decd78da09edbb9919715
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-10-14 21:17:27 +0530

    roboshop project

--- mysql.sh
+++ mysql.sh
@@ -9,10 +9,12 @@ yum install mysql-community-server -y &>>/tmp/roboshop.log
 
 echo -e "\e[32mStart mysql services\e[0m"
 systemctl enable mysqld &>>/tmp/roboshop.log
-systemctl restart mysqld &>>/tmp/roboshop.log
+systemctl start mysqld &>>/tmp/roboshop.log
 
 echo -e "\e[32mChange the default Root Password\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
 
 echo -e "\e[32mCheck Password working or not\e[0m"
 mysql -uroot -pRoboShop@1 &>>/tmp/roboshop.log
+
+systemctl start mysqld &>>/tmp/roboshop.log
