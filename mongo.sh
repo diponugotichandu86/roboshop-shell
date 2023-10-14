b9b0ffcf3e45adc04a4f5c81ae186058d8b87d2f
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-10-14 09:01:48 +0530

    updating mongodb

--- mongo.sh
+++ mongo.sh
@@ -1,7 +1,10 @@
 echo -e "\e[32mInstalling MongoDB Server\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y  -y &>>/tmp/roboshop.log
-#Modify the config file
+
+echo -e "\e[32mUpdate MongoDB Listen Address\e[0m"
+sed -i -e "s/127.0.0.0/0.0.0.0/" /etc/mongodb.conf
+
 echo -e "\e[32mStarting MongoDB Service\e[0m"
 systemctl enable mongod -y &>>/tmp/roboshop.log
 systemctl restart mongod -y &>>/tmp/roboshop.log
